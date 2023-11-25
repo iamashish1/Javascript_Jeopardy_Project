4b0c949feb2a2ff511fc09ca62c0f23244d01bf7
nayeemhust <nayeemhust@gmail.com>
2023-11-24 23:22:02 -0500

    SET

--- js/ashish_nayeem.js
+++ js/ashish_nayeem.js
@@ -6,6 +6,8 @@ var isModalOpen = false;
 var currentPlayer = 1;
 var score1 = 0;
 var score2 = 0;
+var playerName1='';
+var playerName2='';
 document.addEventListener("DOMContentLoaded", function () {
   let categories;
   let questionsArray = [];
@@ -47,10 +49,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     coveredSurface.classList.add("covered-surface");
     //
-    const rightAnswer = document.createElement('button');
+    const rightAnswer = document.createElement('span');
+    rightAnswer.setAttribute('id','right-button')
 
 
-    rightAnswer.innerHTML = '<img src="assets/right.png" width:"20px" alt="Right Answer">';
+    rightAnswer.innerHTML = '<img src="assets/right.png" alt="Right Answer">';
 
     rightAnswer.onclick = function() {
       handleAnswerSubmit(100, true);
@@ -63,7 +66,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    const wrongAnswer = document.createElement('button');
+    const wrongAnswer = document.createElement('span');
+
     wrongAnswer.innerHTML = '<img src="assets/Wrong.png" alt="Wrong Answer">';
     wrongAnswer.onclick = function() {
       handleAnswerSubmit(100, false);
@@ -200,18 +204,63 @@ document.addEventListener("DOMContentLoaded", function () {
 
         playerName1.setAttribute("placeholder", "Player 1");
 
+        //create set suffix button for player1
+        let suffix1= document.createElement('span');
+        suffix1.innerHTML='SET'
+        suffix1.style.backgroundColor="red"
+        suffix1.style.color="white"
+        suffix1.addEventListener('click', function() {
+          let getWhatsInside1= document.getElementById('p1').value;
+          if(getWhatsInside1!=''){
+            let newlyCreated1= document.createElement('span');
+            newlyCreated1.innerHTML=getWhatsInside1+' VERSUS';
+            newlyCreated1.style.color='green';
+            newlyCreated1.style.marginRight='10px';
+
+            gameControlsDiv.replaceChild( newlyCreated1, document.getElementById('p1'));
+         
+         suffix1.remove();
+          }
+
+
+        });
+
         const playerName2 = document.createElement("input");
         playerName2.setAttribute("type", "text");
         playerName2.style.height = "50px";
         playerName2.setAttribute("id", "p2");
 
         playerName2.setAttribute("placeholder", "Player 2");
+         //create set suffix button for player1
+         let suffix2= document.createElement('span');
+         suffix2.style.backgroundColor="red"
+         suffix2.style.color="white"
+
+         suffix2.innerHTML='SET'
+         suffix2.addEventListener('click', function() {
+          // Add your logic here
+          let getWhatsInside2= document.getElementById('p2').value;
+          if(getWhatsInside2!=''){
+            let newlyCreated2= document.createElement('span');
+            newlyCreated2.innerHTML= ' '+getWhatsInside2+' ';
+            newlyCreated2.style.color='green'
+            gameControlsDiv.replaceChild( newlyCreated2, document.getElementById('p2'));
+            suffix2.remove();
+          }
+
+        });
 
         resetButton.onclick = function () {
           if (isModalOpen) {
             return;
           }
           jeopardyBoard.innerHTML = "";
+          document.getElementById("player-2").innerHTML=0;
+          document.getElementById("player-1").innerHTML=0;
+          score1=0;
+          score2=0;
+          currentPlayer=1;
+
 
           buildJeopardyBoard(categories, questions);
         };
@@ -253,7 +302,11 @@ document.addEventListener("DOMContentLoaded", function () {
         gameControlsDiv.appendChild(avatar1);
 
         gameControlsDiv.appendChild(playerName1);
+        gameControlsDiv.appendChild(suffix1);
+
         gameControlsDiv.appendChild(playerName2);
+        gameControlsDiv.appendChild(suffix2);
+
         gameControlsDiv.appendChild(avatar2);
         gameControlsDiv.appendChild(player2Score);
 
@@ -400,5 +453,21 @@ function handleAnswerSubmit(score, isCorrect) {
      document.getElementById('avatar1').style.border="4px solid green";
   }
 
+
+}
+
+function deleteCookie(){
+
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+}
+
+function readCookie(){
+
+let x = document.cookie;
+
+}
+
+function CreateCookie(){
 
 }
